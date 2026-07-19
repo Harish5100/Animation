bc51005fdf537716d849edb23f26176193870dae
Harish D <d.harish902540@gmail.com>
2026-07-19 19:15:54 +0530

    Initialize Flask app with index route

---
+++ api/index.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, render_template
+base_dir = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.abspath(os.path.join(base_dir, '..', 'templates'))
+static_dir = os.path.abspath(os.path.join(base_dir, '..', 'static'))
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+@app.route("/")
+def index():
+    return render_template("index.html")
+if __name__ == "__main__":
+    app.run(debug=True)
